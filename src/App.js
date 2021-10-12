b13bd5b1dd6b7cf54e0c1826776e0459980ea6c1
haiii van <phanvanhai1710@gmail.com>
2021-10-12 17:01:39 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PageNotFound from './containers/PageNotFound';
+import { renderRoutesHome } from "./routes";
+import Navbar from './containers/HomeTemplate/_component/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+      <Navbar />
+      <Switch>
+        {renderRoutesHome()}
+        <Route path="" component={PageNotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
