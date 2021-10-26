6a9996a5fc9bea25c6bd9819b8dbbc604b5b6b79
Quyen <luongtrongq@gmail.com>
2021-10-26 09:17:20 +0700

    2610

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PageNotFound from './containers/PageNotFound';
 import { renderRoutesHome, renderRouteAdmin } from "./routes";
-import Footer from './containers/HomeTemplate/_component/Footer';
+import Footer from './containers/HomeTemplate/_component/Footer'
+
 function App() {
   return (
     <BrowserRouter >
